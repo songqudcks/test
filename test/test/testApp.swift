08672691db9e40f3fc8c10e491206b2573b26edf
songqudcks <songqudcks@gmail.com>
2020-12-07 17:53:44 +0900

    gh

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Song on 2020/12/07.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
